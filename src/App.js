4fa3556aa20c424dbdc83cd7cace14430163aef9
Kushtrim <bujupikushtrim@outlook.com>
2024-05-11 23:08:19 +0200

    useEffect on mount to fetch data from fake server.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { useEffect } from 'react';
 import Header from './Header';
 import Main from './Main';
 
 export default function App() {
+  useEffect(() => {
+    async function fetchQuestions() {
+      try {
+        const response = await fetch('http://localhost:3001/questions');
+        console.log(response);
+        if (!response.ok) throw new Error(`HTTP error ${response.status} | ${response.statusText}`);
+
+        const data = await response.json();
+        console.log(data);
+        // setQuestions(data);
+      } catch (error) {
+        console.error('Error fetching questions:', error);
+      }
+    }
+    fetchQuestions();
+  }, []);
+
   return (
     <div className='app'>
       <Header />
