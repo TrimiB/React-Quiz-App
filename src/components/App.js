06ec1ee3d03e7ad10b02a5a701bc5df0b3d8fd2f
Kushtrim <bujupikushtrim@outlook.com>
2024-05-17 23:28:23 +0200

    creted new Progress componennt and neccessary logic for it

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import Error from './Error';
 import StartScreen from './StartScreen';
 import Question from './Question';
 import NextButton from './NextButton';
+import Progress from './Progress';
 
 const initialState = {
   questions: [],
@@ -42,8 +43,12 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status, index, answer }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce((prev, qur) => prev + qur.points, 0);
 
   useEffect(() => {
     async function fetchQuestions() {
@@ -74,6 +79,13 @@ export default function App() {
         {status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch} />}
         {status === 'active' && (
           <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
             <Question question={questions[index]} answer={answer} dispatch={dispatch} />
             <NextButton dispatch={dispatch} answer={answer} />
           </>
