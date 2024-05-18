310b595891e0f337a6898ebee6e60ae85da8c19f
Kushtrim <bujupikushtrim@outlook.com>
2024-05-19 00:16:28 +0200

    Created Timer and Footer component and implemented timer logic

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,10 @@ import Question from './Question';
 import NextButton from './NextButton';
 import Progress from './Progress';
 import FinishScreen from './FinishScreen';
+import Timer from './Timer';
+import Footer from './Footer';
+
+const SEC_PER_QUESTION = 10;
 
 const initialState = {
   questions: [],
@@ -18,6 +22,7 @@ const initialState = {
   answer: null,
   points: 0,
   highscore: 0,
+  secondsRemaining: null,
 };
 
 function reducer(state, action) {
@@ -27,7 +32,11 @@ function reducer(state, action) {
     case 'dataFailed':
       return { ...state, status: 'error' };
     case 'startQuiz':
-      return { ...state, status: 'active' };
+      return {
+        ...state,
+        status: 'active',
+        secondsRemaining: state.questions.length * SEC_PER_QUESTION,
+      };
     case 'newAnswer':
       const question = state.questions.at(state.index);
 
@@ -47,11 +56,15 @@ function reducer(state, action) {
       };
     case 'reset':
       return {
-        ...state,
+        ...initialState,
+        questions: state.questions,
         status: 'ready',
-        index: 0,
-        answer: null,
-        points: 0,
+      };
+    case 'timer':
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? 'finished' : state.status,
       };
     default:
       throw new Error(`Unhandled action type: ${action.type}`);
@@ -59,10 +72,8 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status, index, answer, points, highscore }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
+  const [{ questions, status, index, answer, points, highscore, secondsRemaining }, dispatch] =
+    useReducer(reducer, initialState);
   const numQuestions = questions.length;
   const maxPossiblePoints = questions.reduce((prev, qur) => prev + qur.points, 0);
 
@@ -103,12 +114,15 @@ export default function App() {
               answer={answer}
             />
             <Question question={questions[index]} answer={answer} dispatch={dispatch} />
-            <NextButton
-              dispatch={dispatch}
-              answer={answer}
-              numQuestions={numQuestions}
-              index={index}
-            />
+            <Footer>
+              <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
+              <NextButton
+                dispatch={dispatch}
+                answer={answer}
+                numQuestions={numQuestions}
+                index={index}
+              />
+            </Footer>
           </>
         )}
         {status === 'finished' && (
---
+++ src/components/Timer.js
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+
+function Timer({ dispatch, secondsRemaining }) {
+  const minutes = Math.floor(secondsRemaining / 60);
+  const seconds = secondsRemaining % 60;
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      dispatch({ type: 'timer' });
+    }, 1000);
+
+    return () => clearInterval(id);
+  }, [dispatch]);
+
+  return (
+    <div className='timer'>
+      {minutes < 10 ? `0${minutes}` : minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+    </div>
+  );
+}
+
+export default Timer;
