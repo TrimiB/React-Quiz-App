234447ac913a3cca051cbac25a8a5a2937988bd0
Kushtrim <bujupikushtrim@outlook.com>
2024-05-18 21:33:28 +0200

    displaying emoji based on percentage

--- src/components/FinishScreen.js
+++ src/components/FinishScreen.js
@@ -1,8 +1,22 @@
 function FinnishScreen({ points, maxPossiblePoints }) {
   const procentage = Math.ceil((points / maxPossiblePoints) * 100);
+
+  let emoji;
+  if (procentage >= 90) emoji = '🥇';
+
+  if (procentage >= 70) emoji = '🥈';
+
+  if (procentage >= 50) emoji = '🥉';
+
+  if (procentage >= 30) emoji = '🤨';
+
+  if (procentage >= 10) emoji = '🫤';
+
+  if (procentage === 0) emoji = '🤦‍♂️';
+
   return (
     <p className='result'>
-      You scored {points} out of {maxPossiblePoints} points. {procentage}%
+      <span>{emoji}</span> You scored {points} out of {maxPossiblePoints} points. {procentage}%
     </p>
   );
 }
