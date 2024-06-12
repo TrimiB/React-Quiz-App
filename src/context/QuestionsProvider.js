61507ad9a77725ddff867fdcb00f0a241d8a1240
Kushtrim <bujupikushtrim@outlook.com>
2024-06-12 21:12:28 +0200

    [Rafactor] Creating Questions Context

--- src/context/QuestionsProvider.js
+++ src/context/QuestionsProvider.js
@@ -1,9 +1,107 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useEffect, useReducer } from 'react';
 
 const QuestionsContext = createContext();
 
+const SEC_PER_QUESTION = 20;
+
+const initialState = {
+  questions: [],
+  status: 'loading', // 'loading', 'error', 'ready', 'active', 'finished'
+  index: 0,
+  answer: null,
+  points: 0,
+  highscore: 0,
+  secondsRemaining: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataRecieved':
+      return { ...state, questions: action.payload, status: 'ready' };
+    case 'dataFailed':
+      return { ...state, status: 'error' };
+    case 'startQuiz':
+      return {
+        ...state,
+        status: 'active',
+        secondsRemaining: state.questions.length * SEC_PER_QUESTION,
+      };
+    case 'newAnswer':
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption ? state.points + question.points : state.points,
+      };
+    case 'nextQuestion':
+      return { ...state, index: state.index + 1, answer: null };
+    case 'finished':
+      return {
+        ...state,
+        status: 'finished',
+        highscore: state.points > state.highscore ? state.points : state.highscore,
+      };
+    case 'reset':
+      return {
+        ...initialState,
+        questions: state.questions,
+        status: 'ready',
+      };
+    case 'timer':
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? 'finished' : state.status,
+      };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
 function QuestionsProvider({ children }) {
-  return <QuestionsContext.Provider value={{}}>{children}</QuestionsContext.Provider>;
+  const [{ questions, status, index, answer, points, highscore, secondsRemaining }, dispatch] =
+    useReducer(reducer, initialState);
+  const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce((prev, qur) => prev + qur.points, 0);
+
+  useEffect(() => {
+    async function fetchQuestions() {
+      try {
+        const response = await fetch('http://localhost:3001/questions');
+        // console.log(response);
+        if (!response.ok) throw new Error(`HTTP error ${response.status} | ${response.statusText}`);
+
+        const data = await response.json();
+        // console.log(data);
+        // setQuestions(data);
+        dispatch({ type: 'dataRecieved', payload: data });
+      } catch (error) {
+        // console.error('Error fetching questions:', error);
+        dispatch({ type: 'dataFailed' });
+      }
+    }
+    fetchQuestions();
+  }, []);
+
+  return (
+    <QuestionsContext.Provider
+      value={{
+        questions,
+        status,
+        index,
+        answer,
+        points,
+        highscore,
+        secondsRemaining,
+        numQuestions,
+        maxPossiblePoints,
+        dispatch,
+      }}
+    >
+      {children}
+    </QuestionsContext.Provider>
+  );
 }
 
 function useQuestions() {
